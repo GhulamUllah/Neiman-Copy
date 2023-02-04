1fe27365662820a5cf85a1e3b165ca52f049f2e6
Ghulam Ullah <ghulamullahkhan84@gmail.com>
2023-02-04 09:50:23 +0500

    Success

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
 import { Typography} from '@mui/material'
+import './index.css'
+import {BrowserRouter,RouteProps,Route} from 'react-router-dom'
 
 import Nonauth from './components/header/nonauth';
 import Category from './components/category';
 import Mainpage from './components/mainpage';
+import { Routes } from 'react-router-dom/dist';
+import Tailwindcss from './components/tailwindcss';
 function App() {
   return (
     <div className="App">
-      <div className='update' style={{background:'black',width:'100%',color:'#fff', textAlign:'center', padding:3}}>
+     <BrowserRouter>
+     {/* <div className='update' style={{background:'black',width:'100%',color:'#fff', textAlign:'center', padding:3}}>
         <Typography sx={{fontWeight:'bolder'}}>The New Neiman Marcus app is here! Discover Now</Typography>
       </div>
       <Nonauth/>
       <Category/>
-      <Mainpage/>
+      <Mainpage/> */}
+    
+      <Routes>
+        <Route path='/ghulamullah' element={<Tailwindcss/>}/>
+      </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/tailwindcss.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Tailwindcss() {
+  return (
+    <div className='w-full bg-black text-white text-center h-screen flex align-center'>T</div>
+  )
+}
